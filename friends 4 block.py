74712e674f567903889d098b1a80a812731a27fe
scio <ghsehr1@gmail.com>
2018-09-13 22:58:34 +0900

    2nd upload_modifiy
    friends 4 block modifiy

--- friends 4 block.py
+++ friends 4 block.py
@@ -1,13 +1,13 @@
-from pprint import pprint as p
 def block_check(m,n,board):
     sustainable=0
-    temp=0
     for i in range(m-1):
         for j in range(n-1):
             if board[i][j]==None or board[i+1][j]==None or board[i + 1][j+1]==None or board[i][j+1]==None:
+                # print(i,j,board[i][j]==None,board[i+1][j]==None)
                 pass
             else:
                 check=[]
+                # print(board[i][j])
                 check.append(board[i][j].lower() == board[i][j + 1].lower())
                 check.append(board[i][j].lower() == board[i + 1][j].lower())
                 check.append(board[i][j].lower() == board[i + 1][j + 1].lower())
@@ -17,7 +17,6 @@ def block_check(m,n,board):
                     board[i][j+1] = board[i][j+1].lower()
                     board[i+1][j] = board[i+1][j].lower()
                     board[i+1][j+1] = board[i+1][j+1].lower()
-
     return board, sustainable
 
 def score_check(m, n, b,score):
@@ -31,22 +30,9 @@ def score_check(m, n, b,score):
 
     return score,b
 
-def item_pull_down(m,n,b):
-    for i in range(n):
-        for j in range(m - 1, -1, -1):
-            temp = b[j][i]
-            if temp == None:
-                for k in range(j, -1, -1):
-                    if b[k][i] == None:
-                        pass
-                    else:
-                        b[j][i] = b[k][i]
-                        b[k][i] = None
-                        break
-    return b
-
 def solution(m, n, board):
     score = 0
+
     # str to list
     b = []
     for i in range(m):
@@ -56,21 +42,23 @@ def solution(m, n, board):
         b.append(temp)
 
     while True:
+        # 4 block check
         b,sustainable = block_check(m, n, b)
+        p(b)
         score,b=score_check(m,n,b,score)
+
         if not sustainable:
             break
-        item_pull_down(m,n,b)
+
+        for i in range(n):
+            for j in range(m-1,-1,-1):
+                temp=b[j][i]
+                if temp==None:
+                    for k in range(j,-1,-1):
+                        if b[k][i]==None:
+                            pass
+                        else:
+                            b[j][i]=b[k][i]
+                            b[k][i]=None
+                            break
     return score
-
-list=[
-    "TTTANT",
-    "RRFACC",
-    "RRRFCC",
-    "TRRRAA",
-    "TTMMMF",
-    "TMMTTJ"]
-
-p(solution(6,6,list))
-
-
