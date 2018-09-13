9309505353316c493b78394fa9e1e02e20e3c18c
scio <ghsehr1@gmail.com>
2018-09-13 23:02:32 +0900

    2nd upload_modifiy
    friends 4 block modifiy

--- friends 4 block.py
+++ friends 4 block.py
@@ -3,11 +3,9 @@ def block_check(m,n,board):
     for i in range(m-1):
         for j in range(n-1):
             if board[i][j]==None or board[i+1][j]==None or board[i + 1][j+1]==None or board[i][j+1]==None:
-                # print(i,j,board[i][j]==None,board[i+1][j]==None)
                 pass
             else:
                 check=[]
-                # print(board[i][j])
                 check.append(board[i][j].lower() == board[i][j + 1].lower())
                 check.append(board[i][j].lower() == board[i + 1][j].lower())
                 check.append(board[i][j].lower() == board[i + 1][j + 1].lower())
@@ -27,8 +25,21 @@ def score_check(m, n, b,score):
             elif (b[i][j].islower())==True:
                 score+=1
                 b[i][j]=None
-
     return score,b
+
+def item_pull_down(m,n,b):
+    for i in range(n):
+        for j in range(m - 1, -1, -1):
+            temp = b[j][i]
+            if temp == None:
+                for k in range(j, -1, -1):
+                    if b[k][i] == None:
+                        pass
+                    else:
+                        b[j][i] = b[k][i]
+                        b[k][i] = None
+                        break
+    return b
 
 def solution(m, n, board):
     score = 0
@@ -42,23 +53,9 @@ def solution(m, n, board):
         b.append(temp)
 
     while True:
-        # 4 block check
         b,sustainable = block_check(m, n, b)
-        p(b)
         score,b=score_check(m,n,b,score)
-
         if not sustainable:
             break
-
-        for i in range(n):
-            for j in range(m-1,-1,-1):
-                temp=b[j][i]
-                if temp==None:
-                    for k in range(j,-1,-1):
-                        if b[k][i]==None:
-                            pass
-                        else:
-                            b[j][i]=b[k][i]
-                            b[k][i]=None
-                            break
+        item_pull_down(m,n,b)
     return score
